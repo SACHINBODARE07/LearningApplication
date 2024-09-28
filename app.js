4aca97986a7fce15d5de95b4f59877275f6142fe
Sachin Shankar Bodare <94038587+SACHINBODARE07@users.noreply.github.com>
2024-09-28 19:33:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -126,3 +126,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Student Signup Backend API');
+});
+
